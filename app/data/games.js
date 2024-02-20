0add65dd8816d08179d394d78775e5e1b30812da
gustavo <gustavo.cesariosilva15@gmail.com>
2024-02-20 09:12:29 -0300

    Cover GoW PS4

--- app/data/games.js
+++ app/data/games.js
@@ -4,9 +4,9 @@ const games = [
     { id: 3, title: 'God of War: Chains of Olympus', platform: 'PlayStation Portable', year: 2008, cover: 'https://upload.wikimedia.org/wikipedia/en/3/3a/God_of_War_Chains_of_Olympus_NA_version_front_cover.jpg' },
     { id: 4, title: 'God of War III', platform: 'PlayStation 3', year: 2010, cover: 'https://upload.wikimedia.org/wikipedia/pt/6/6c/God_of_War_3_capa.png' },
     { id: 5, title: 'God of War: Ghost of Sparta', platform: 'PlayStation Portable', year: 2010, cover: 'https://upload.wikimedia.org/wikipedia/en/2/26/GOW_Ghost_of_Sparta_boxart.jpg' },
-    { id: 6, title: 'God of War: Ascension', platform: 'PlayStation 3', year: 2013 },
-    { id: 7, title: 'God of War (2018)', platform: 'PlayStation 4', year: 2018 },
-    { id: 8, title: 'God of War: Ragnarok', platform: 'PlayStation 5', year: 2022 }
+    { id: 6, title: 'God of War: Ascension', platform: 'PlayStation 3', year: 2013, cover: 'https://upload.wikimedia.org/wikipedia/en/b/ba/God_of_War_Ascension.jpg' },
+    { id: 7, title: 'God of War (2018)', platform: 'PlayStation 4', year: 2018, cover: 'https://upload.wikimedia.org/wikipedia/en/a/a7/God_of_War_4_cover.jpg' },
+    { id: 8, title: 'God of War: Ragnarok', platform: 'PlayStation 5', year: 2022, cover: 'https://upload.wikimedia.org/wikipedia/en/e/ee/God_of_War_Ragnar%C3%B6k_cover.jpg' }
   ];
   
   module.exports = games;  
