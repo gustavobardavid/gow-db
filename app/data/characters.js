8255f52d23dbe75098eda7f6e1bb3bff95d70c3d
gustavo <gustavo.cesariosilva15@gmail.com>
2024-02-20 16:51:12 -0300

    Covers

--- app/data/characters.js
+++ app/data/characters.js
@@ -1,5 +1,5 @@
 const characters = [
-  { id: 1, name: 'Kratos', role: 'Protagonist', weapon: 'Blades of Chaos' },
+  { id: 1, name: 'Kratos', role: 'Protagonist', weapon: 'Blades of Chaos', cover: 'https://i.pinimg.com/564x/b6/1f/19/b61f191483f1fd109fe922f2b4419853.jpg' },
   { id: 2, name: 'Ares', role: 'Antagonist', weapon: 'God of War' },
   { id: 3, name: 'Zeus', role: 'Antagonist', weapon: 'Lightning Bolts' },
   { id: 4, name: 'Athena', role: 'Supporting', weapon: 'Divine Wisdom' },
--- app/data/games.js
+++ app/data/games.js
@@ -1,11 +1,11 @@
 const games = [
-    { id: 1, title: 'God of War', platform: 'PlayStation 2', year: 2005, cover: 'https://upload.wikimedia.org/wikipedia/pt/5/53/God_of_War_2005_capa.png' },
-    { id: 2, title: 'God of War II', platform: 'PlayStation 2', year: 200, cover: 'https://upload.wikimedia.org/wikipedia/pt/7/7e/God_of_War_2_capa.png' },
+    { id: 1, title: 'God of War', platform: 'PlayStation 2', year: 2005, cover: 'https://i.pinimg.com/564x/b0/a6/1c/b0a61c3a3be8c2626e7a20d5ff9408bd.jpg' },
+    { id: 2, title: 'God of War II', platform: 'PlayStation 2', year: 200, cover: 'https://i.pinimg.com/564x/7b/4f/d6/7b4fd6df1d423ee35744e90a1c5cdf3d.jpg' },
     { id: 3, title: 'God of War: Chains of Olympus', platform: 'PlayStation Portable', year: 2008, cover: 'https://upload.wikimedia.org/wikipedia/en/3/3a/God_of_War_Chains_of_Olympus_NA_version_front_cover.jpg' },
-    { id: 4, title: 'God of War III', platform: 'PlayStation 3', year: 2010, cover: 'https://upload.wikimedia.org/wikipedia/pt/6/6c/God_of_War_3_capa.png' },
-    { id: 5, title: 'God of War: Ghost of Sparta', platform: 'PlayStation Portable', year: 2010, cover: 'https://upload.wikimedia.org/wikipedia/en/2/26/GOW_Ghost_of_Sparta_boxart.jpg' },
-    { id: 6, title: 'God of War: Ascension', platform: 'PlayStation 3', year: 2013, cover: 'https://upload.wikimedia.org/wikipedia/en/b/ba/God_of_War_Ascension.jpg' },
-    { id: 7, title: 'God of War (2018)', platform: 'PlayStation 4', year: 2018, cover: 'https://upload.wikimedia.org/wikipedia/en/a/a7/God_of_War_4_cover.jpg' },
+    { id: 4, title: 'God of War III', platform: 'PlayStation 3', year: 2010, cover: 'https://i.pinimg.com/564x/d8/87/3b/d8873b8943ae2582a2b4eee4a86db664.jpg' },
+    { id: 5, title: 'God of War: Ghost of Sparta', platform: 'PlayStation Portable', year: 2010, cover: 'https://i.pinimg.com/564x/86/97/87/86978707bce1151f1834af9174e9fa83.jpg' },
+    { id: 6, title: 'God of War: Ascension', platform: 'PlayStation 3', year: 2013, cover: 'https://i.pinimg.com/564x/3a/62/a4/3a62a4becd659c4c54cbab3a20ab9b15.jpg' },
+    { id: 7, title: 'God of War (2018)', platform: 'PlayStation 4', year: 2018, cover: 'https://i.pinimg.com/564x/55/35/0f/55350fcb8d7ef24f02e0ff04de83e4fc.jpg' },
     { id: 8, title: 'God of War: Ragnarok', platform: 'PlayStation 5', year: 2022, cover: 'https://upload.wikimedia.org/wikipedia/en/e/ee/God_of_War_Ragnar%C3%B6k_cover.jpg' }
   ];
   
