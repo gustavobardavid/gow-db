e0da73ef5a00d8014683c04df5a8ef40d035ae26
gustavo <gustavo.cesariosilva15@gmail.com>
2024-02-20 16:57:58 -0300

    Characters Cover

--- app/data/characters.js
+++ app/data/characters.js
@@ -1,9 +1,9 @@
 const characters = [
   { id: 1, name: 'Kratos', role: 'Protagonist', weapon: 'Blades of Chaos', cover: 'https://i.pinimg.com/564x/b6/1f/19/b61f191483f1fd109fe922f2b4419853.jpg' },
-  { id: 2, name: 'Ares', role: 'Antagonist', weapon: 'God of War' },
-  { id: 3, name: 'Zeus', role: 'Antagonist', weapon: 'Lightning Bolts' },
-  { id: 4, name: 'Athena', role: 'Supporting', weapon: 'Divine Wisdom' },
-  { id: 5, name: 'Poseidon', role: 'Antagonist', weapon: 'Trident' },
+  { id: 2, name: 'Ares', role: 'Antagonist', weapon: 'God of War', cover: 'https://i.pinimg.com/564x/26/0a/18/260a182d30cc2399742b4ebaceb59143.jpg' },
+  { id: 3, name: 'Zeus', role: 'Antagonist', weapon: 'Lightning Bolts', cover: 'https://i.pinimg.com/564x/7c/e7/de/7ce7de9a8830d3741d3429f58ec771e7.jpg' },
+  { id: 4, name: 'Athena', role: 'Supporting', weapon: 'Divine Wisdom', cover: 'https://i.pinimg.com/564x/77/07/57/770757c00e4ef51098abe8b2462a6286.jpg' },
+  { id: 5, name: 'Poseidon', role: 'Antagonist', weapon: 'Trident', cover: 'https://i.pinimg.com/564x/e8/a8/09/e8a8092515c23588f857fd6a67c288de.jpg' },
   { id: 6, name: 'Hades', role: 'Antagonist', weapon: 'Claws of Hades' },
   { id: 7, name: 'Hercules', role: 'Antagonist', weapon: 'Gauntlets of Hercules' },
   { id: 8, name: 'Helios', role: 'Supporting', weapon: 'Sun Shield' },
